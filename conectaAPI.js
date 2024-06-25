fd20407654abbe2306b359a02189da10e7de8a0f
DiegoAAranda <116285740+DiegoAAranda@users.noreply.github.com>
2024-06-24 18:11:59 -0600

    Add files via upload

---
+++ conectaAPI.js
@@ -0,0 +1,52 @@
+export async function listaProductos() {
+    const conexion = await fetch("http://localhost:3001/productos", {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json"
+        }
+    });
+
+    if (!conexion.ok) {
+        throw new Error("No se pudo obtener la lista de productos");
+    }
+
+    const productos = await conexion.json();
+    return productos;
+}
+
+export async function crearProducto(nombre, imagen, precio) {
+    const conexion = await fetch("http://localhost:3001/productos", {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            nombre: nombre,
+            imagen: imagen,
+            precio: precio
+        })
+    });
+
+    if (!conexion.ok) {
+        throw new Error("No fue posible enviar el producto");
+    }
+
+    const productoCreado = await conexion.json();
+    return productoCreado;
+}
+
+export async function eliminarProducto(id) {
+    const conexion = await fetch(`http://localhost:3001/productos/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (!conexion.ok) {
+        throw new Error("No fue posible eliminar el producto");
+    }
+
+    const resultado = await conexion.json();
+    return resultado;
+}
---
+++ mostrarProducto.js
@@ -0,0 +1,83 @@
+import { listaProductos, crearProducto, eliminarProducto } from "./conectaAPI.js";
+
+const form = document.querySelector("[data-form]");
+const lista = document.querySelector("[data-lista]");
+
+// Contruye los productos
+function construyeCard(nombre, imagen, precio, id) {
+    const producto = document.createElement("li");
+    producto.className = "producto__item";
+
+    producto.innerHTML = `
+        <div class="imagen-producto">
+            <img src="${imagen}" alt="${nombre}">
+        </div>
+        <div class="descripcion-producto">
+            <h3>${nombre}</h3>
+            <p>Precio: $${precio}</p>
+        </div>
+        <button class="eliminar-producto" data-id="${id}">Eliminar Producto</button>`;
+
+    return producto;
+}
+
+//Muetra la lista de productos
+async function mostrarProductos() {
+    try {
+        const productos = await listaProductos();
+        lista.innerHTML = ""; // Limpiar lista antes de agregar nuevos productos
+
+        productos.forEach(producto => {
+            lista.appendChild(construyeCard(producto.nombre, producto.imagen, producto.precio, producto.id));
+        });
+    } catch (error) {
+        lista.innerHTML = `<h2 class="mensaje__titulo">No fue posible cargar la lista de productos</h2>`;
+        console.error("Error al cargar productos:", error);
+    }
+}
+
+// Función para limpiar inputs
+function limpiarFormulario() {
+    form.reset();
+}
+
+// Envío del formulario
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const nombre = document.querySelector("[data-name]").value;
+    const precio = document.querySelector("[data-price]").value;
+    const imagen = document.querySelector("[data-image]").value;
+
+    try {
+        const nuevoProducto = await crearProducto(nombre, imagen, precio);
+        console.log("Producto creado:", nuevoProducto);
+
+        // Limpiar el formulario
+        limpiarFormulario();
+
+        // Actualizar la lista
+        mostrarProductos();
+    } catch (error) {
+        console.error("Error al crear el producto:", error);
+    }
+});
+
+// "Eliminar Producto"
+lista.addEventListener("click", async (event) => {
+    if (event.target.classList.contains("eliminar-producto")) {
+        const id = event.target.dataset.id;
+
+        try {
+            await eliminarProducto(id);
+            console.log("Producto eliminado:", id);
+
+            // Actualizar la lista de productos después de eliminar uno
+            mostrarProductos();
+        } catch (error) {
+            console.error("Error al eliminar el producto:", error);
+        }
+    }
+});
+
+// Mostrar la lista de productos al cargar la página
+mostrarProductos();
